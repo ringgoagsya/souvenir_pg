9ddc2c60b6e201f3b7cac8a17bdf60a1c856644b
ringgoagsya <63402411+ringgoagsya@users.noreply.github.com>
2020-12-10 01:28:05 +0700

    Update connect.php

--- souvenir_bkt/admin/inc/connect.php
+++ souvenir_bkt/admin/inc/connect.php
@@ -1,10 +1,10 @@
 <?php
 
-	$host = "ec2-184-72-235-159.compute-1.amazonaws.com";
-	$user = "sjtnwkwqhxklku";
-	$pass = "4d0b992db8431e4331287ab0430b51871509ed25a1b5feb3e9764ae1a78a6e2c";
+	$host = "ec2-18-206-103-49.compute-1.amazonaws.com";
+	$user = "essyzgnvwgwepl";
+	$pass = "0417854c3ee064568312fd87cb0ba5a683c82c967f227736d2d5a23059d246ac";
 	$port = "5432";
-	$dbname = "dbehfdvl0f5kuv";
+	$dbname = "dab9lvt6rmfttf";
 
 	$conn = pg_connect("host=".$host." port=".$port." dbname=".$dbname." user=".$user." password=".$pass) or die("Gagal");
 ?>
